4598eb9a8f9c19400cf654a43e98185ab13148ef
takakiyo <takakiyo@gmail.com>
2015-10-08 13:48:37 +0800

    first commit

---
+++ netbean/JFXSM2/src/com/axis/security/ECPointCompressionFlag.java
@@ -0,0 +1,13 @@
+package com.axis.security;
+
+/**
+ * @author axis
+ * @date 2015年9月30日
+ */
+public enum ECPointCompressionFlag {
+
+    None, Compression, HyBird;
+
+    private ECPointCompressionFlag() {
+    }
+}
